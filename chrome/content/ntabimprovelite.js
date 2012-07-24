b21aed1e08a2486f13edc87af629d7873ac50ec8
jiaofeng <jiaofeng601@gmail.com>
2012-07-24 16:13:31 +0800

    add install button on addon-bar
    Signed-off-by: jiaofeng <jiaofeng601@gmail.com>

--- chrome/content/ntabimprovelite.js
+++ chrome/content/ntabimprovelite.js
@@ -45,11 +45,35 @@ var ntabimprovelite = {
     this._tabOpeningOptions();
     this._tabClosingOptions();
     this._sendStats();
+    this.installButton("ntabimprove");
   },
 
   get _eTLDService() {
     delete this._eTLDService;
     return this._eTLDService = Cc["@mozilla.org/network/effective-tld-service;1"].getService(Ci.nsIEffectiveTLDService);
+  },
+  installButton: function ZoomPanel__installButton(buttonId,toolbarId) {
+    toolbarId = toolbarId || "addon-bar";
+    var key = "extensions.toolbarbutton.installed."+buttonId;
+    if(Application.prefs.getValue(key, false))
+      return;
+    
+  	var toolbar = window.document.getElementById(toolbarId);
+  	let curSet = toolbar.currentSet;
+  	if (-1 == curSet.indexOf(buttonId)){
+  		let newSet = curSet + "," + buttonId;
+  		toolbar.currentSet = newSet;
+  		toolbar.setAttribute("currentset", newSet);
+  		document.persist(toolbar.id, "currentset");
+  		try{
+  			BrowserToolboxCustomizeDone(true);
+  		}catch(e){}
+  	}
+  	if (toolbar.getAttribute("collapsed") == "true") {
+  		toolbar.setAttribute("collapsed", "false");
+  	}
+  	document.persist(toolbar.id, "collapsed");
+  	Application.prefs.setValue(key, true);
   },
 
   getDomainFromURI: function(aURI) {
